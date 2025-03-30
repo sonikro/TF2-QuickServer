89ac5ac5f8de344968f56a28acb082b1785255af
Jonathan Nagayoshi <jonathan@nagayoshi.com.br>
2025-03-30 23:08:56 +0000

    feat(TerminateServer): implements delete server command

---
+++ src/entrypoints/commands/TerminateServer/handler.test.ts
@@ -0,0 +1,88 @@
+import { Chance } from "chance";
+import { ChatInputCommandInteraction } from "discord.js";
+import { describe, expect, it, vi } from "vitest";
+import { mock } from "vitest-mock-extended";
+import { when } from "vitest-when";
+import { ServerManager } from "../../../application/services/ServerManager";
+import { terminateServerHandlerFactory } from "./handler";
+import { Region } from "../../../domain";
+
+describe("terminateServerCommandHandler", () => {
+    const chance = new Chance();
+
+    const createHandler = () => {
+        const serverManager = mock<ServerManager>();
+        const interaction = mock<ChatInputCommandInteraction>();
+        interaction.options = mock();
+
+        const handler = terminateServerHandlerFactory({
+            serverManager
+        });
+
+        return {
+            serverManager,
+            interaction,
+            handler,
+        };
+    };
+
+
+    it("should terminate the server with the specified server ID and Region", async () => {
+        // Given
+        const { handler, interaction, serverManager } = createHandler();
+        const serverId = chance.guid();
+        const region = chance.pickone(Object.values(Region))
+
+        when(interaction.options.getString)
+            .calledWith("server_id")
+            .thenReturn(serverId);
+
+        when(interaction.options.getString)
+            .calledWith("region")
+            .thenReturn(region);
+
+        when(serverManager.deleteServer)
+            .calledWith({ serverId, region })
+            .thenResolve();
+
+        // When
+        await handler(interaction);
+
+        // Then
+        expect(serverManager.deleteServer).toHaveBeenCalledWith({
+            serverId,
+            region
+        });
+        expect(interaction.reply).toHaveBeenCalledWith(
+            `Server has been terminated.`,
+        );
+    });
+
+    it("should reply with an error if the server termination fails", async () => {
+        // Given
+        const { handler, interaction, serverManager } = createHandler();
+        const serverId = chance.guid();
+        const region = chance.pickone(Object.values(Region))
+
+        when(interaction.options.getString)
+            .calledWith("server_id")
+            .thenReturn(serverId);
+
+        when(interaction.options.getString)
+            .calledWith("region")
+            .thenReturn(region);
+
+        when(serverManager.deleteServer)
+            .calledWith({ serverId, region })
+            .thenReject(new Error("Termination failed"));
+
+
+        // When
+        await handler(interaction);
+
+        // Then
+        expect(interaction.reply).toHaveBeenCalledWith(
+            'Failed to terminate server. Please contact the administrator.'
+        );
+    });
+});
--- src/entrypoints/commands/TerminateServer/handler.ts
+++ src/entrypoints/commands/TerminateServer/handler.ts
@@ -1,14 +1,25 @@
 import { ChatInputCommandInteraction } from "discord.js";
 import { ServerManager } from "../../../application/services/ServerManager";
+import { Region } from "../../../domain";
 
 export function terminateServerHandlerFactory(dependencies: {
     serverManager: ServerManager
 }) {
     return async function terminateServerCommandHandler(interaction: ChatInputCommandInteraction) {
         const serverId = interaction.options.getString('server_id');
+        const region = interaction.options.getString('region');
 
-        // Logic for terminating server
-        await interaction.reply(`Server with ID ${serverId} has been terminated.`);
+        try {
+            await dependencies.serverManager.deleteServer({
+                region: region as Region,
+                serverId: serverId!
+            })
+    
+            await interaction.reply(`Server has been terminated.`);
+        } catch (error) {
+            console.error("Error terminating server:", error);
+            await interaction.reply(`Failed to terminate server. Please contact the administrator.`);
+        }
     }
 
 }
