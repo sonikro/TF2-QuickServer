f0a02c58ed7de1b6029997c66ab53fa50f1352fc
Jonathan Nagayoshi <jonathan@nagayoshi.com.br>
2025-04-03 03:29:01 +0000

    feat(infra): starts to setup map infrastructure

--- src/infrastructure/cdk/bin/tf2-quickserver.ts
+++ src/infrastructure/cdk/bin/tf2-quickserver.ts
@@ -1,31 +1,64 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { Tf2QuickServerStack } from '../lib/tf2-quickserver-stack';
 import { Tags } from 'aws-cdk-lib';
-import { config } from 'dotenv';
 import appConfig from 'config';
+import { config } from 'dotenv';
+import { TF2GlobalStack } from '../lib/tf2-global-stack';
+import { TF2RegionalStack } from '../lib/tf2-regional-stack';
 
 // Load environment variables from .env file
 config();
 
+// Initialize the CDK app
 const app = new cdk.App();
 
 // Add default tags to all resources
 Tags.of(app).add('Project', 'TF2-QuickServer');
 
-// Get regions from the configuration
-const regionsConfig = appConfig.get<Record<string, { enabled: boolean }>>("aws.regions");
+// Deploy the Global Maps Stack
+const globalStack = createGlobalMapsStack(app);
+// Deploy regional stacks based on configuration
+createRegionalStacks(app, [globalStack]);
 
-for (const [region, { enabled }] of Object.entries(regionsConfig)) {
-  if (!enabled) continue;
+// Deploy the Map Files Stack
 
-  // Deploy a stack for each enabled region
-  new Tf2QuickServerStack(app, `Tf2QuickServerStack-${region}`, {
+/**
+ * Creates the Global Maps Stack.
+ */
+function createGlobalMapsStack(app: cdk.App): TF2GlobalStack {
+  return new TF2GlobalStack(app, 'Tf2MapsStack', {
+    crossRegionReferences: true,
     env: {
       account: process.env.AWS_ACCOUNT_ID,
-      region: region,
+      region: 'us-east-1', // Global resources should be in us-east-1
     },
   });
+}
 
-  console.log(`Stack created for region: ${region}`);
+/**
+ * Creates regional stacks based on the configuration.
+ */
+function createRegionalStacks(app: cdk.App, dependsOn: cdk.Stack[]): cdk.Stack[] {
+  const regionsConfig = appConfig.get<Record<string, { enabled: boolean }>>('aws.regions');
+  const regionalStacks: cdk.Stack[] = [];
+
+  for (const [region, { enabled }] of Object.entries(regionsConfig)) {
+    if (!enabled) continue;
+
+    const quickServerStack = new TF2RegionalStack(app, `Tf2QuickServerStack-${region}`, {
+      env: {
+        account: process.env.AWS_ACCOUNT_ID,
+        region: region,
+      },
+    });
+
+    dependsOn.forEach((stack) => {
+      quickServerStack.addDependency(stack);
+    })
+
+    regionalStacks.push(quickServerStack);
+    console.log(`Stack created for region: ${region}`);
+  }
+
+  return regionalStacks;
 }
