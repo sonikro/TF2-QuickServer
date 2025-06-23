efe9c0cd6f5638435a62ca6e417e75abac408f35
Jonathan Nagayoshi <jonathan@nagayoshi.com.br>
2025-06-23 01:56:12 +0000

    fix(ban): prevents banned players from opening servers

--- src/core/repository/UserBanRepository.ts
+++ src/core/repository/UserBanRepository.ts
@@ -1,9 +1,9 @@
 export interface UserBanRepository {
   /**
    * Checks if a user is banned by SteamID or DiscordID.
-   * @param steamId The user's SteamID (string)
+   * @param steamId3 The user's SteamID3
    * @param discordUserId The user's Discord user ID (string | undefined)
    * @returns Promise<{ isBanned: boolean, reason?: string }> - isBanned true if banned, false otherwise, and optional reason
    */
-  isUserBanned(steamId: string, discordUserId?: string): Promise<{ isBanned: boolean; reason?: string }>;
+  isUserBanned(steamId3: string, discordUserId?: string): Promise<{ isBanned: boolean; reason?: string }>;
 }
