d4d42f232782003f71ca4126945c6c8f4d253aa6
Jonathan Nagayoshi <jonathan@nagayoshi.com.br>
2025-06-25 14:17:53 +0000

    refact(udpListener): makes it more resilient to failures

--- src/entrypoints/udp/srcdsCommandListener.ts
+++ src/entrypoints/udp/srcdsCommandListener.ts
@@ -1,24 +1,11 @@
-import { parseSRCDSCommand } from "./srcdsCommands";
+import { ResilientLogReceiver } from "./ResilientLogReceiver";
 import { UDPCommandsServices } from "./srcdsCommands/UDPCommandServices";
-import { LogReceiver } from "@c43721/srcds-log-receiver";
 
 export async function startSrcdsCommandListener(dependencies: UDPCommandsServices) {
-    const receiver = new LogReceiver({
+    new ResilientLogReceiver({
         address: "0.0.0.0",
         port: process.env.SRCDS_COMMAND_LISTENER_PORT ? Number(process.env.SRCDS_COMMAND_LISTENER_PORT) : 27100,
+        services: dependencies,
     });
-
-    console.log("SRCDS Log receiver running.. ");
-
-    receiver.on("event", async ({ message, password }: { message: string, password: string }) => {
-        const command = parseSRCDSCommand(message);
-        if (command) {
-            try {
-                await command.handler({ args: command.args, password, services: dependencies });
-            } catch (error) {
-                console.error(`[SRCDS Command Handler Error] ${error}`);
-            }
-        }
-    });
-
+    console.log("[SRCDS Command Listener] Started listening for commands.");
 }
